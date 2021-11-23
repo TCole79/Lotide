0e8536338bf14a191aa8b1f562b2ac565ee04ef4
Tristan Cole <tristanleighcole@googlemail.com>
2021-11-23 02:48:19 +0000

    assertEqual file with emoji

---
+++ assertEqual.js
@@ -0,0 +1,20 @@
+function assertEqual(actual, expected) {
+  let valueA = actual;
+  let valueB = expected;
+
+  if (valueA === valueB) {
+    console.log("\uD83D\uDE00 \uD83D\uDE00 \uD83D\uDE4F Assertion Passed: [actual] === [expected]");
+  } else {
+    console.log("Assertion Failed: [actual] !=== [expected]");
+  }
+
+}
+
+// test code:
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("zim", "zim");
+assertEqual("zim", "zimmer");
+assertEqual(613, 613);
+
+//let emoji = String.fromCodePoint(0x1F621)
+//"\uD83D\uDE00"
