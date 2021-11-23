96bdfc07e27346b6bf0a7e16a4d64b3013fb31a8
Tristan Cole <tristanleighcole@googlemail.com>
2021-11-23 03:15:19 +0000

    head function with assertEqual function inc.

---
+++ head.js
@@ -0,0 +1,18 @@
+function assertEqual(actual, expected) {
+  let valueA = actual;
+  let valueB = expected;
+
+  if (valueA === valueB) {
+    console.log(`\uD83D\uDE00 \uD83D\uDE00 \uD83D\uDE4F Assertion Passed: ${valueA} === ${valueB}`);
+  } else {
+    console.log(`Assertion Failed: ${valueA} !=== ${valueB}`);
+  }
+}
+
+function head(array) {
+  return array[0];
+}
+
+// test code:
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
