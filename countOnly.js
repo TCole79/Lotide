cbfe342be03fe7bacb4d39d6acae2c82b16d2229
Tristan Cole <tristanleighcole@googlemail.com>
2021-11-25 00:42:51 +0000

    countOnly for lotide

---
+++ countOnly.js
@@ -0,0 +1,57 @@
+function assertEqual(actual, expected) {
+  let valueA = actual;
+  let valueB = expected;
+
+  if (valueA === valueB) {
+    console.log(`\uD83D\uDE00 \uD83D\uDE00 \uD83D\uDE4F Assertion Passed: ${valueA} === ${valueB}`);
+  } else {
+    console.log(`:octagonal_sign: Assertion Failed: ${valueA} !=== ${valueB}`);
+  }
+}
+// test code:
+/*
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("zim", "zim");
+assertEqual("zim", "zimmer");
+assertEqual(613, 613);
+*/
+
+// countOnly will be given an array and an object. It will return an object containing counts of everything that the input object listed.
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    console.log(item);
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+}
+//Add an if condition to only increment our count in results if the item is found in the itemsToCount object.
+// Increment the count of each item into results as we encounter each string item in the array.
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
