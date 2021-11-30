8cd587ee7d2a25e60a68b648403af12920c4eb94
Tristan Cole <tristanleighcole@googlemail.com>
2021-11-30 02:58:14 +0000

    lotide test files, with mocha for headTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -15,3 +15,17 @@ console.log(middle([1, 2, 3, 4])); // => [2, 3]
 console.log(middle([2, 1, 3, 4, 5, 6])); // => [3, 4]
 
 console.log(assertArraysEqual([1, 2, 3] , [1, 2, 3]), true);
+
+
+// const assert = require('chai').assert;
+// const middle = require('../middle');
+
+// describe('#middle', () => {
+//   it("returns 1 for [1, 2, 3]", () => {
+//   assert.strictEqual(middle([1, 2, 3]), 1);
+//   });
+
+//   it("returns '5' for ['5']", () => {
+//     assert.strictEqual(middle(['5']), '5');
+//   });
+// });
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,24 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
+
+
+
+
+
+/* advanced solution with the mentor:
+const assert = require('chai').assert;
+const tail = require('../tail');
+const assertArraysEqual = require('../assertArraysEqual'); // we brought this in, to use assertArraysEqual in the code below
+
+describe('#tail', () => {
+  it("returns a new array without index 0, but everything else", () => {
+  assert.strictEqual(assertArraysEqual(tail([1, 2, 3]), [1, 2, 3]), false );
+  console.log([1, 2, 3]);
+  }); */
