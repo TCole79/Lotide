e01a2193c5c0bdc26bec6fb07c75cdb421d3b11b
Tristan Cole <tristanleighcole@googlemail.com>
2021-11-30 03:22:10 +0000

    tail and middle refactor with mocha + chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,31 +1,36 @@
-const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
-
-//test code:
-//For arrays with one or two elements, there is no middle. Return an empty array.
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-
-//For arrays with odd number of elements, an array containing a single middle element should be returned.
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 5, 3, 4, 2])); // => [3]
-
-//For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([2, 1, 3, 4, 5, 6])); // => [3, 4]
-
-console.log(assertArraysEqual([1, 2, 3] , [1, 2, 3]), true);
-
-
-// const assert = require('chai').assert;
 // const middle = require('../middle');
+// const assertArraysEqual = require('../assertArraysEqual');
 
-// describe('#middle', () => {
-//   it("returns 1 for [1, 2, 3]", () => {
-//   assert.strictEqual(middle([1, 2, 3]), 1);
-//   });
+// //test code:
+// //For arrays with one or two elements, there is no middle. Return an empty array.
+// console.log(middle([1])); // => []
+// console.log(middle([1, 2])); // => []
 
-//   it("returns '5' for ['5']", () => {
-//     assert.strictEqual(middle(['5']), '5');
-//   });
-// });
+// //For arrays with odd number of elements, an array containing a single middle element should be returned.
+// console.log(middle([1, 2, 3])); // => [2]
+// console.log(middle([1, 5, 3, 4, 2])); // => [3]
+
+// //For arrays with an even number of elements, an array containing the two elements in the middle should be returned
+// console.log(middle([1, 2, 3, 4])); // => [2, 3]
+// console.log(middle([2, 1, 3, 4, 5, 6])); // => [3, 4]
+
+// console.log(assertArraysEqual([1, 2, 3] , [1, 2, 3]), true);
+
+
+
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+describe('#middle', () => {
+  it("returns an empty array if there are only 1 or 2 elements", () => {
+  assert.equal(middle([1, 2]), '');
+  });
+
+  it("returns the middle element in an odd array", () => {
+    assert.equal(middle([1, 2, 3, 4, 5]), '3');
+  });
+
+  it("returns an array containing the middle two elements in an even array", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,21 +1,30 @@
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+
+// // Test Case: Check the original array
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
+
 const assertEqual = require('../assertEqual');
-const tail = require('../tail');
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-
-
-
-
-
-
-/* advanced solution with the mentor:
 const assert = require('chai').assert;
 const tail = require('../tail');
-const assertArraysEqual = require('../assertArraysEqual'); // we brought this in, to use assertArraysEqual in the code below
+// Test Case: Check the original array
+
+it("returns [2, 3] for [1, 2, 3]", () => {
+  assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+});
+
+
+
+
+/* advanced solution with the mentor (because I skipped ahead by mistake):
+const assert = require('chai').assert;
+const tail = require('../tail');
+const assertArraysEqual = require('../assertArraysEqual'); // we brought this in to use assertArraysEqual in the code below
 
 describe('#tail', () => {
   it("returns a new array without index 0, but everything else", () => {
