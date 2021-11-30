ac26862a4cf133998bc83cdeb892ef3b6904e7e6
Tristan Cole <tristanleighcole@googlemail.com>
2021-11-30 04:18:09 +0000

    updated index file

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const tail = require('./tail');
 const middle = require('./middle');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
-const assertObjectsEqual = require('./assertObjectsEqual');
-const countLetters = require('./countLetters');
-const countOnly = require('./countOnly');
 const eqArrays = require('./eqArrays');
-const eqObjects = require('./eqObjects');
-const findKey = require('./findKey');
-const findKeyByValue = require('./findKeyByValue');
-const letterPositions = require('./letterPositions');
-const map = require('./map');
-const takeUntil = require('./takeUntil');
-const without = require('./without');
+// const assertObjectsEqual = require('./assertObjectsEqual');
+// const countLetters = require('./countLetters');
+// const countOnly = require('./countOnly');
+// const eqObjects = require('./eqObjects');
+// const findKey = require('./findKey');
+// const findKeyByValue = require('./findKeyByValue');
+// const letterPositions = require('./letterPositions');
+// const map = require('./map');
+// const takeUntil = require('./takeUntil');
+// const without = require('./without');
 
 
 module.exports = {
@@ -23,15 +23,15 @@ module.exports = {
   middle: middle,
   assertArraysEqual: assertArraysEqual,
   assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without
+  eqArrays: eqArrays
+  // assertObjectsEqual: assertObjectsEqual,
+  // countLetters: countLetters,
+  // countOnly: countOnly,
+  // eqObjects: eqObjects,
+  // findKey: findKey,
+  // findKeyByValue: findKeyByValue,
+  // letterPositions: letterPositions,
+  // map: map,
+  // takeUntil: takeUntil,
+  // without: without
 };
